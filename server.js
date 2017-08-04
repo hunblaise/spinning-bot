bdd637196143b152715704ead0b33afcf22a357e
Balazs Hajdu <v-bhajdu@hotels.com>
2017-08-04 18:05:04 +0200

    no issue

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ server.listen(process.env.port || process.env.PORT || 3978, function () {
   console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create chat connector for communicating with the Bot Framework Service
+// Create chat connector for communicating with the bot framework service
 var connector = new builder.ChatConnector({
   appId: process.env.MICROSOFT_APP_ID,
   appPassword: process.env.MICROSOFT_APP_PASSWORD
